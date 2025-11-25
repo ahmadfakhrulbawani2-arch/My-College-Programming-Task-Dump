bbae449b254aea6a603357aeeac635650d706fed
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-25 11:04:24 +0700

    Add files

---
+++ Tugas 1_Pola Silang & modul 0/main1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    long long a = 0, b = 0, c = 0;
+
+    scanf("%lld %lld %lld", &a, &b, &c);
+
+    if(a <= 0 || b <= 0 || c <= 0) {
+        printf("Gomenasai, segitiga tidak dapat dibuat.");
+    } else if(a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b) {
+        printf("Yatta, aku bisa membentuk segitiga!");
+    } else {
+        printf("Gomenasai, segitiga tidak dapat dibuat.");
+    }
+    return 0;
+}
