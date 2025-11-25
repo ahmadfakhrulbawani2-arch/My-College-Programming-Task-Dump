afb0e0636294bc71381a72f0c05102185b0f5ff1
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-25 11:09:41 +0700

    Add files

---
+++ Tugas 2_Latsol Modul 1/main5.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char n[100];
+    for(int i = 0; i < strlen(n); ++i) {
+        scanf("%c", &n[i]);
+    }
+
+    int A = 0, I = 0, E = 0, U = 0, O = 0, A = 0, B = 0, C = 0, D = 0, F = 0, G = 0, H = 0, J = 0, K = 0, L = 0, M = 0, N = 0, O = 0, P = 0, Q = 0, R = 0, S = 0, T = 0, U = 0, V = 0, W = 0, X = 0, Y = 0, Z = 0;
+    for(int i = 0; i < strlen(n); ++i) {
+        switch(n[i]) {
+            case 'a':
+                ++A;
+                break;
+            case 'b':
+                ++B;
+                break;
+            case 'c':
+                ++C;
+                break;
+            case 'd':
+                ++D;
+                break;
+            case 'e':
+                ++E;
+                break;
+            case 'f':
+                ++F;
+                break;
+            case 'g':
+                ++G;
+                break;
+            case 'h':
+                ++H;
+                break;
+            case 'i':
+                ++I;
+                break;
+            case 'j':
+                ++J;
+                break;
+            case 'k':
+                ++K;
+                break;
+            case 'l':
+                ++L;
+                break;
+            case 'm':
+                ++M;
+                break;
+            case 'n':
+                ++N;
+                break;
+            case 'o':
+                ++O;
+                break;
+            case 'p':
+                ++P;
+                break;
+            case 'q':
+                ++Q;
+                break;
+            case 'r':
+                ++R;
+                break;
+            case 's':
+                ++S;
+                break;
+            case 't':
+                ++T;
+                break;
+            case 'u':
+                ++U;
+                break;
+            case 'v':
+                ++V;
+                break;
+            case 'w':
+                ++W;
+                break;
+            case 'x':
+                ++X;
+                break;
+            case 'y':
+                ++Y;
+                break;
+            case 'z':
+                ++Z;
+                break;
+        }
+    }
+    printf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d")
+    return 0;
+}
