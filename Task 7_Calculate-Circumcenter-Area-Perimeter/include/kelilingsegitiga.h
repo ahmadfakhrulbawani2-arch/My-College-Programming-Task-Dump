11a9d5735926bf987cd1edd53472b8dc2147bb75
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-29 10:36:09 +0700

    Updated Nov 29, 2025

---
+++ Task 7_Calculate-Circumcenter-Area-Perimeter/include/kelilingsegitiga.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+#ifndef KELILINGSEGITIGA_H
+#define KELILINGSEGITIGA_H
+
+#define ll long long
+
+// in kelilingsegitiga.h
+double calcDis(ll x1, ll y1, ll x2, ll y2);
+
+#endif
