8336eb46e9dde3edf5ff44315a389c17385c2923
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-29 10:36:05 +0700

    Updated Nov 29, 2025

---
+++ Task 7_Calculate-Circumcenter-Area-Perimeter/include/circumcenter.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+#ifndef CIRCUMCENTER_H
+#define CIRCUMCENTER_H
+
+#define ll long long
+
+// in circumcenter.h
+double calcCircumX(ll x1, ll x2, ll x3, ll y1, ll y2, ll y3);
+double calcCircumY(ll x1, ll x2, ll x3, ll y1, ll y2, ll y3);
+
+#endif
