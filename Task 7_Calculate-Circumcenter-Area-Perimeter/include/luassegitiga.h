a38328c10e4f21e30d168e0879d8b42c93b7cf61
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-29 10:36:12 +0700

    Updated Nov 29, 2025

---
+++ Task 7_Calculate-Circumcenter-Area-Perimeter/include/luassegitiga.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+#ifndef LUASSEGITIGA_H
+#define LUASSEGITIGA_H
+
+#define ll long long
+
+// in luassegitiga.h
+double calcArea(ll x1, ll x2, ll x3, ll y1, ll y2, ll y3);
+
+#endif
