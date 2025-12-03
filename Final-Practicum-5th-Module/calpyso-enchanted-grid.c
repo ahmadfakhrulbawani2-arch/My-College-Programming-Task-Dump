abe825473e6e4957077240f90e09a054da3ebc07
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-12-03 16:01:40 +0700

    Added Dec 03, 2025

---
+++ Final-Practicum-5th-Module/calpyso-enchanted-grid.c
@@ -0,0 +1,73 @@
+// the explanation is in the bottom
+// Bismillahirrahmanirrahim
+// dengan menyebut nama Allah Subhanahuwata'ala yang Mahapengasih lagi Mahapenyayang.
+
+// Header doang banyak, tapi gak dipake awokawokawoawok
+#include <assert.h>
+#include <complex.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fenv.h>
+#include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdalign.h>
+#include <stdarg.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <tgmath.h>
+#include <time.h>
+#include <wchar.h>
+#include <wctype.h>
+
+//  Terusin tambahin define terus sampai meleduck wkkwkkkwkwkw
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int usi;
+#define POSINF 100000000
+#define NEGINF -100000000
+#define FNDMIN(a, b) ((a < b) ? (a) : (b))
+#define FNDMAX(a, b) ((a > b) ? (a) : (b))
+#define ASCIIa 97
+#define ASCIIz 122
+#define ASCIIA 65
+#define ASCIIZ 90
+
+void solve() {
+    int x1, y1, x2, y2;
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    int res;
+    if(x1 == x2 || y1 == y2) {
+        res = 1;
+        printf("%d\n", res);
+        return;
+    }
+
+    int diffX = abs(x2 - x1);
+    int diffY = abs(y2 - y1);
+
+    res = diffX + diffY;
+
+    printf("%d\n", res);
+}
+
+int main(void) {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
