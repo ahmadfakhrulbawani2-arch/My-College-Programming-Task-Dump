bae8446637244a018d0129ac5ba16da01a51c431
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-12-05 22:24:51 +0700

    Added Dec 5, 2025

---
+++ Final-Practicum-5th-Module/glass-bridge.c
@@ -0,0 +1,65 @@
+// the explanation is in the bottom
+// Bismillahirrahmanirrahim
+// dengan menyebut nama Allah Subhanahuwata'ala yang Mahapengasih lagi Mahapenyayang.
+
+// Header doang banyak, tapi gak dipake awokawokawoawok
+#include <assert.h>
+#include <complex.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fenv.h>
+#include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdalign.h>
+#include <stdarg.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <tgmath.h>
+#include <time.h>
+#include <wchar.h>
+#include <wctype.h>
+
+//  Terusin tambahin define terus sampai meleduck wkkwkkkwkwkw
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int usi;
+#define POSINT 100000000
+#define NEGINT -100000000
+#define NEGLL -1000000000000000000
+#define POSLL 1000000000000000000
+#define FNDMIN(a, b) ((a < b) ? (a) : (b))
+#define FNDMAX(a, b) ((a > b) ? (a) : (b))
+#define DECISI(a, b) ((a > b) ? (1) : (2))
+#define REP2(i, m, n) for(ll i = m; i <= n; i++) // change i with var. name.
+#define REP1(i, m, n) for(ll i = m; i < n; i++) // change i with var. name.
+#define ASCIIa 97
+#define ASCIIz 122
+#define ASCIIA 65
+#define ASCIIZ 90
+#define ASCII0 48
+#define ASCII9 57
+#define AWAL int main(void) {
+#define AKHIR return 0;}
+
+AWAL
+
+int n;
+scanf("%d", &n);
+
+char jawaban[] = {'r', 'l', 'r', 'l', 'l', 'r', 'l', 'l', 'r', 'l', 'r', 'r', 'l', 'r', 'l', 'l', 'r', 'r', 'r', 'l', 'l', 'r', 'r', 'r', 'r', 'r', 'l', 'r', 'r', 'l', 'r', 'r', 'r', 'l', 'l', 'l', 'r', 'l', 'l', 'r', 'r', 'r', 'r', 'r', 'l', 'r', 'l', 'l', 'l', 'r', 'r', 'l', 'r', 'l', 'l', 'l', 'r', 'l', 'l', 'l', 'r', 'r', 'l', 'r', 'l', 'l', 'l', 'r', 'l', 'l', 'l', 'r', 'r', 'l', 'l', 'l', 'r', 'l', 'l', 'r', 'l', 'l', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'l', 'r', 'l', 'l', 'l', 'r', 'r', 'l', 'r', 'r', 'l'}; // #7
+
+printf("%c", jawaban[n]);
+
+AKHIR
