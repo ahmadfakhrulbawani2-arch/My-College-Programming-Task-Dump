f7ba4f80adbbe6113e1d0f9bdbcf3943d9379274
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-12-05 22:31:27 +0700

    Last-Commit-FP-Dasprog-ALHAMDULILLAHH

---
+++ Task-8_Input-Data/struct.h
@@ -0,0 +1,13 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+typedef struct _penduduk {
+    char nama[40];
+    short tahun_lahir;
+    short bulan_lahir;
+    short tanggal_lahir;
+    char desa[20];
+    char kecamatan[20];
+} penduduk;
+
+#endif
